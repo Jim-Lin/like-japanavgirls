45fcbb77d8a7701f78d054c5c841e858bbd8ae6d
Jim-Lin <acgsong.tw@yahoo.com.tw>
2016-12-20 10:12:29 +0800

    Modify to https

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+import tornado.httpserver
 import tornado.ioloop
 from handler import WebHookHandler
 
@@ -9,5 +10,9 @@ application = tornado.web.Application([
 ])
 
 if __name__ == "__main__":
-    application.listen(5000)
+    http_server = tornado.httpserver.HTTPServer(application, ssl_options={
+        "certfile": "/etc/tornado/ssl/tornado.csr",
+        "keyfile": "/etc/tornado/ssl/tornado.key",
+    })
+    http_server.listen(443)
     tornado.ioloop.IOLoop.instance().start()
