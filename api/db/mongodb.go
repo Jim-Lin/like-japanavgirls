fe8cfbd35e47dd90b9d6ea29090d4a2407eaa820
Jim-Lin <acgsong.tw@yahoo.com.tw>
2016-12-27 23:30:48 +0800

    Add init func

--- api/db/mongodb.go
+++ api/db/mongodb.go
@@ -6,99 +6,22 @@ import (
 )
 
 const (
-	Host         = "mongodb://localhost:27017"
-	Database     = "dark"
-	AuthDatabase = ""
-	AuthUserName = ""
-	AuthPassword = ""
+	Host     = "mongodb://localhost:27017"
+	Database = "dark"
 )
 
 var (
-	mainSession *mgo.Session
-	mainDb      *mgo.Database
+	Db *mgo.Database
 )
 
-type MgoDb struct {
-	Session *mgo.Session
-	Db      *mgo.Database
-	Col     *mgo.Collection
-}
-
 func init() {
-	if mainSession == nil {
-		mainSession, err := mgo.Dial(Host)
-		if err != nil {
-			panic(err)
-		}
-
-		mainSession.SetMode(mgo.Monotonic, true)
-		mainDb = mainSession.DB(Database)
-	}
+	session, _ := mgo.Dial(Host)
+	Db = session.DB(Database)
 }
 
-func (this *MgoDb) Init() *mgo.Session {
-
-	this.Session = mainSession.Copy()
-	this.Db = this.Session.DB(Database)
-
-	return this.Session
-}
-
-func (this *MgoDb) C(collection string) *mgo.Collection {
-	this.Col = this.Session.DB(Database).C(collection)
-	return this.Col
-}
-
-func (this *MgoDb) Close() bool {
-	defer this.Session.Close()
-	return true
-}
-
-func (this *MgoDb) DropoDb() {
-	err := this.Session.DB(Database).DropDatabase()
-	if err != nil {
-		panic(err)
-	}
-}
-
-func (this *MgoDb) RemoveAll(collection string) bool {
-	this.Session.DB(Database).C(collection).RemoveAll(nil)
-
-	this.Col = this.Session.DB(Database).C(collection)
-	return true
-}
-
-func (this *MgoDb) Index(collection string, keys []string) bool {
-
-	index := mgo.Index{
-		Key:        keys,
-		Unique:     true,
-		DropDups:   true,
-		Background: true,
-		Sparse:     true,
-	}
-	err := this.Db.C(collection).EnsureIndex(index)
-	if err != nil {
-		panic(err)
-
-		return false
-	}
-
-	return true
-}
-
-func (this *MgoDb) IsDup(err error) bool {
-
-	if mgo.IsDup(err) {
-		return true
-	}
-
-	return false
-}
-
-func (this *MgoDb) FindOneActress(id string) map[string]string {
+func FindOneActress(id string) map[string]string {
 	result := map[string]string{}
-	if err := this.C("actress").Find(bson.M{"id": id}).One(&result); err != nil {
+	if err := Db.C("actress").Find(bson.M{"id": id}).Select(bson.M{"name": 1, "img": 1, "_id": 0}).One(&result); err != nil {
 		panic(err)
 	}
 
