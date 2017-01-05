a1a77c4203d1585698d09e612c2df2aa22745434
Jim-Lin <acgsong.tw@yahoo.com.tw>
2017-01-05 16:09:40 +0800

    Add api proxy_pass

--- chrome-extension/info.js
+++ chrome-extension/info.js
@@ -2,7 +2,7 @@
     var filename;
 
     var feedback = function(id, ox) {
-        var url = "http://like-av.xyz:9090/feedback";
+        var url = "http://like-av.xyz/api/feedback";
         var xhr = new XMLHttpRequest();
 
         xhr.open("POST", url, true);
@@ -132,7 +132,7 @@
         var loading = document.getElementById("loading");
         loading.style.display = "block";
 
-        var url = "http://like-av.xyz:9090/upload";
+        var url = "http://like-av.xyz/api/upload";
         var xhr = new XMLHttpRequest();
         var fd = new FormData();
         xhr.open("POST", url, true);
