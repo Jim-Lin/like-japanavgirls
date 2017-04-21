1a856d2fa3b9ba40c4c5032330b904a75f8d1e64
Jim-Lin <acgsong.tw@yahoo.com.tw>
2017-04-21 15:08:19 +0800

    Add dark-classifier module

---
+++ dark-classifier/job/scheduler.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from etl import ETL
+
+if __name__ == "__main__":
+    etl = ETL()
+
+    # monthly
+    etl.check_monthly_ranking()
+
+    etl.check_new_actress()
+    
+    # daily
+    etl.check_new_works()
