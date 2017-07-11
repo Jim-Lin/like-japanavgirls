7dcea19f2cc73c18a12b64478b2a94929479d80f
Jim-Lin <acgsong.tw@yahoo.com.tw>
2017-07-11 17:43:57 +0800

    Modify text

--- web/localizations.js
+++ web/localizations.js
@@ -1,16 +1,16 @@
 String.toLocaleString({
 	"en": {
-		"%sub-title": "search the sexy actress that the girl is similar to",
+		"%sub-title": "search the actress that the girl is similar to",
 		"%upload-description": "please upload beauty girl image",
 		"%error": "not found, please upload another image",
 		"%buy": "SHOPPING",
 		"%similarity": "similarity",
-		"%like": "O LIKE",
-		"%unlike": "X UNLIKE"
+		"%like": "LIKE",
+		"%unlike": "UNLIKE"
 	},
 	"zh": {
-		"%sub-title": "人不可貌相之女優明星臉",
-		"%upload-description": "請上傳妹紙圖片",
+		"%sub-title": "女優明星臉",
+		"%upload-description": "請上傳正妹圖片",
 		"%error": "找不到, 請上傳其他圖片",
 		"%buy": "買片去",
 		"%similarity": "相識度",
