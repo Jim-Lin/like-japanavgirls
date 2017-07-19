7c2d5bd3ca1637776d4f50a6ec75cdb38a6305a0
Jim-Lin <acgsong.tw@yahoo.com.tw>
2017-07-19 21:45:28 +0800

    Add decodeURIComponent to url

--- chrome-extension/background.js
+++ chrome-extension/background.js
@@ -11,7 +11,7 @@ chrome.contextMenus.onClicked.addListener(onClickHandler);
 
 function onClickHandler(info, tab) {
     // The srcUrl property is only available for image elements.
-    var url = 'info.html#' + info.srcUrl;
+    var url = 'info.html#' + decodeURIComponent(info.srcUrl);
 
     // Create a new window to the info page.
     chrome.windows.create({ url: url, width: 560, height: 560, type: "popup" });
